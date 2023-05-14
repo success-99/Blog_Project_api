9062a5cd17cb99f4e994313e6e313828594104d5
mbp13 <shaxobiddina20@gmail.com>
2023-05-14 16:14:54 +0500

    home page change

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
